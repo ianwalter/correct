5a8699480c44b15df7956a9a29340213e56ec069
Ian Walter <public@iankwalter.com>
2019-10-20 23:42:55 -0400

    Refactoring, adding tests

---
+++ index.js
@@ -0,0 +1,11 @@
+const Validation = require('./lib/Validation')
+const ValidationError = require('./lib/ValidationError')
+const SchemaValidator = require('./lib/SchemaValidator')
+const validators = require('./lib/validators')
+
+module.exports = {
+  Validation,
+  ValidationError,
+  SchemaValidator,
+  ...validators
+}
